50d01f568e98d53d245634ce2849a4ec5256ae83
Andrej Chomutovskij <andrej.chomutovskij@gmail.com>
2023-09-10 19:37:02 +0400

    fixes to docker

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -7,10 +7,12 @@ DEST=build/docker
 
 rm -rf $DEST
 mkdir -p $DEST/portfolio-service-server/var/conf
+mkdir -p $DEST/portfolio-service-server/var/certs
 
 cp ./scripts/Dockerfile $DEST/
 tar -xf "./portfolio-service-server/build/distributions/portfolio-service-server-${VERSION}.tar" -C $DEST/portfolio-service-server --strip-components=1
 cp ./portfolio-service-server/var/conf/conf.yml $DEST/portfolio-service-server/var/conf
+cp ./portfolio-service-server/var/certs/* $DEST/portfolio-service-server/var/certs
 
 cd $DEST
 docker build -t "chomutovskij/portfolio-service-server:$VERSION" .
